229105dfe09dc177a883a6b6271b4855a138b2e2
Andrew Silva <andrewsilva9@gmail.com>
2019-05-24 02:17:40 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -10,3 +10,5 @@ class Triangle
     if (@length_one <= 0) || (@length_two <= 0) || (@length_three <= 0)
       raise TriangleError
     elsif (@length_one+@length_two <= @length_three) || (@length_one+@length_three <= @length_two) || (@length_two+@length_three <= @length_one)
+      raise TriangleError
+      
