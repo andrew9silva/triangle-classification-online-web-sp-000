2ec3ac514d6c5eacc3014b6c79c6fb76467ee51f
Andrew Silva <andrewsilva9@gmail.com>
2019-05-24 02:21:38 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -11,4 +11,17 @@ class Triangle
       raise TriangleError
     elsif (@length_one+@length_two <= @length_three) || (@length_one+@length_three <= @length_two) || (@length_two+@length_three <= @length_one)
       raise TriangleError
-      
+    else
+      if (@length_one == @length_two) && (@length_two == @length_three)
+        :equilateral
+      elsif (@length_one == @length_two) || (@length_one == @length_three) || (@length_two == @length_three)
+        :isosceles
+      else (@length_one != @length_two) && (@length_one != @length_three) && (@length_two != @length_three)
+        :scalene
+      end
+    end
+  end
+end
+
+class TriangleError < StandardError
+end
